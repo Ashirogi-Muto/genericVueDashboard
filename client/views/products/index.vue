891264eb3d9914b7d1a7f8183d9869c348586dea
Yogesh Jadhav <ygamer7@gmail.com>
2017-06-28 22:16:33 +0530

    File upload on drag and drop working v1

--- client/views/products/index.vue
+++ client/views/products/index.vue
@@ -57,6 +57,12 @@
             <p class="control">
               <input type="file" @change="onFileChange">
             </p>
+            <p>
+              <div class="dropbox">
+                <input type="file" multiple @change="onFileChange" accept="image/*" class="input-file">
+                <p>Drag files to upload</p>
+              </div>
+            </p>
             <p class="control">
               <button class="button is-primary">Submit</button>
               <button class="button is-link">Cancel</button>
@@ -228,4 +234,34 @@ export default {
     }
   }
 }
+
+.dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; /* minimum height */
+    position: relative;
+    cursor: pointer;
+  }
+
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    cursor: pointer;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
+
 </style>
